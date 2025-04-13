8c2477d02a2a104127e97fa657b19a7baa016122
MoisesEspinozaISM <moises.espinoza@ism.global>
2025-04-13 16:54:58 -0500

    update dag 2

--- dags/dbt_clientes_dag.py
+++ dags/dbt_clientes_dag.py
@@ -9,19 +9,25 @@ default_args = {
 with DAG(
     dag_id='dbt_clientes_pipeline',
     default_args=default_args,
-    schedule_interval=None,  # o ponlo como '0 6 * * *' para diario a las 6am
+    schedule_interval=None,  # Puedes cambiarlo a '0 6 * * *' para ejecución diaria a las 6 AM
     catchup=False,
-    tags=["dbt", "clientes"]
+    tags=["dbt", "clientes"],
+    description="Pipeline para ejecutar y testear modelos DBT del dominio 'clientes'"
 ) as dag:
 
+    # Ejecuta modelos del dominio clientes
     dbt_run_clientes = BashOperator(
         task_id='dbt_run_clientes',
-        bash_command='cd /opt/airflow/dbt && dbt run --select clientes'
+        bash_command='cd /opt/airflow/dbt && dbt run --select clientes',
+        dag=dag
     )
 
+    # Testea los modelos luego de la ejecución
     dbt_test_clientes = BashOperator(
         task_id='dbt_test_clientes',
-        bash_command='cd /opt/airflow/dbt && dbt test --select clientes'
+        bash_command='cd /opt/airflow/dbt && dbt test --select clientes',
+        dag=dag
     )
 
+    # Define el flujo de tareas
     dbt_run_clientes >> dbt_test_clientes
