59905a39d9b61e4b4a582827220066bb98714f0c
MoisesEspinozaISM <moises.espinoza@ism.global>
2025-04-13 16:49:41 -0500

    update nuew dag

--- dags/dbt_clientes_dag.py
+++ dags/dbt_clientes_dag.py
@@ -3,21 +3,17 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 default_args = {
-    'owner': 'airflow',
     'start_date': datetime(2024, 1, 1),
-    'retries': 1,
 }
 
 with DAG(
     dag_id='dbt_clientes_pipeline',
     default_args=default_args,
-    description='Pipeline de clientes con dbt',
-    schedule_interval='@daily',
+    schedule_interval=None,  # o ponlo como '0 6 * * *' para diario a las 6am
     catchup=False,
-    tags=['dbt', 'clientes'],
+    tags=["dbt", "clientes"]
 ) as dag:
 
-    # Ejecuta solo los modelos dentro de models/clientes/
     dbt_run_clientes = BashOperator(
         task_id='dbt_run_clientes',
         bash_command='cd /opt/airflow/dbt && dbt run --select clientes'
